915b5bb811cb9ef2784b2b864712d938c0711840
Haim Cohen <haim1979@gmail.com>
2025-06-27 19:56:43 +0300

    Vercel Analytics Setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import { FavoritesProvider } from '@/contexts/FavoritesContext'
 import SessionProvider from '@/contexts/SessionProvider'
+import { Analytics } from '@vercel/analytics/next'
 
 const inter = Inter({ 
   subsets: ['latin'],
@@ -130,6 +131,7 @@ export default function RootLayout({
             </div>
           </FavoritesProvider>
         </SessionProvider>
+        <Analytics />
       </body>
     </html>
   )
